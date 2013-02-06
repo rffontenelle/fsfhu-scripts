4a9cc936cb4cf0a287abb419cd6ee1d5ff0d7776
Gabor Kelemen <kelemeng@gnome.hu>
2013-02-06 10:30:04 +0100

    Add moz->tx uploader script - v0.0

---
+++ moz-l10n-hu/txin.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+for j in "dtd" "properties" "xhtml"; do
+#	echo "ma épp: $j";
+	case "$j" in
+	 "dtd")
+		TYPE="DTD";
+	;;
+	 "properties")
+		TYPE="MOZILLAPROPERTIES";
+	;;
+	 "xhtml")
+		TYPE="XHTML";
+	;;
+	esac
+	for i in `find mozilla-en/ -name "*$j"`; do 
+#		echo "kezd" ;
+		FILE=`echo $i | cut -d "/" -f "2-"`;
+		RES=`echo $i | cut -d "/" --output-delimiter="_" -f "2-" | tr . _`;
+		tx set --auto-local -r mozilla-hu.$RES "mozilla-<lang>/$FILE" --source-lang en -t $TYPE --execute;
+#		echo "ez kész: $RES - $FILE"
+	done
+#	echo "ez kész: $TYPE";
+done
+
