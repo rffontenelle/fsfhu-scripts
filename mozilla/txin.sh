4969edd4c2bdd6c0d5f1c5a524030b05a7544688
Gabor Kelemen <kelemeng@gnome.hu>
2013-02-21 10:57:51 +0100

    Move txin to the rest of scripts

---
+++ mozilla/txin.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Source the config file that contains the working directory names
+. ~/.config/moz2tx.cfg
+
+cd $MOZTXDIR;
+
+for j in "dtd" "properties"; do
+	case "$j" in
+	 "dtd")
+		TYPE="DTD";
+	;;
+	 "properties")
+		TYPE="MOZILLAPROPERTIES";
+	;;
+	esac
+	for i in `find mozilla-en/ -name "*$j" | grep -v -f blacklist.txt`; do 
+		FILE=`echo $i | cut -d "/" -f "2-"`;
+		RES=`echo $i | cut -d "/" --output-delimiter="_" -f "2-" | tr . _`;
+		tx set --auto-local -r mozilla-hu."$RES" "mozilla-<lang>/$FILE" --source-lang en -t "$TYPE" --execute;
+	done
+done
+
