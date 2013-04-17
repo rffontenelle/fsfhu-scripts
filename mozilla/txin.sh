ea92cc34ac12e18b6920e00376ce9d7810ba84fe
Gabor Kelemen <kelemeng@gnome.hu>
2013-04-17 02:36:10 +0200

    Support blacklist and custom project name

--- mozilla/txin.sh
+++ mozilla/txin.sh
@@ -14,10 +14,10 @@ for j in "dtd" "properties"; do
 		TYPE="MOZILLAPROPERTIES";
 	;;
 	esac
-	for i in `find mozilla-en/ -name "*$j"`; do 
+	for i in `find mozilla-en/ -name "*$j" | grep -v -f ~/.config/txin-blacklist.cfg`; do 
 		FILE=`echo $i | cut -d "/" -f "2-"`;
 		RES=`echo $i | cut -d "/" --output-delimiter="_" -f "2-" | tr . _`;
-		tx set --auto-local -r mozilla-hu."$RES" "mozilla-<lang>/$FILE" --source-lang en -t "$TYPE" --execute;
+		tx set --auto-local -r mozilla-"$LL"."$RES" "mozilla-<lang>/$FILE" --source-lang en -t "$TYPE" --execute;
 	done
 done
 
