2f4955562c5f411b1d161409f877823f40cc3a63
Gabor Kelemen <kelemeng@gnome.hu>
2014-03-07 03:45:52 +0100

    Update liedetector for 3.12

--- gnome/liedetector.sh
+++ gnome/liedetector.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /tmp
-wget http://l10n.gnome.org/languages/C/gnome-3-10/ui/
+wget http://l10n.gnome.org/languages/C/gnome-3-12/ui/
 grep \/module\/[a-z] index.html | cut -d "/" -f3 | sort -u > /tmp/lies.txt
-wget http://ftp.acc.umu.se/pub/GNOME/teams/releng/3.9.2/versions
+wget http://ftp.acc.umu.se/pub/GNOME/teams/releng/3.11.91/versions
 cut -d : -f2 versions | grep -v ^# | sort -u > /tmp/versions.txt 
 diff -u  /tmp/lies.txt /tmp/versions.txt
